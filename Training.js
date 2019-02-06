1c4d356d4e7b3bd8acc3343ec8ae35c6e5a11682
Eric Kanski <erickanski0402@gmail.com>
2019-02-05 19:27:19 -0600

    Added documentation to Training.js

--- Training.js
+++ Training.js
@@ -2,9 +2,8 @@ class Point{
   constructor(){
     this.x = random(-1,1);
     this.y = random(-1,1);
+    //Random coordinates set
 
-    this.px = 0;
-    this.py = 0;
     this.bias = 1;
 
     this.lineY = this.f(this.x);
@@ -17,6 +16,7 @@ class Point{
 
   f(x){
     //y = mx + b
+    //The equation the perceptron is chasing
     return 0.3*x + 0.2;
   }
 
@@ -30,22 +30,27 @@ class Point{
 
   pixelX(){
     return map(this.x, -1,1,0, width);
+    //Maps the coordinates to a cartesian plane
   }
 
   pixelY(){
     return map(this.y, -1,1,height, 0);
+    //Maps the coordinates to a cartesian plane
   }
 
   show(){
     stroke(0);
     if(this.label == 1){
       fill(255);
+      //Positive examples
     }else{
       fill(0);
+      //Negative examples
     }
 
-    this.px = this.pixelX();
-    this.py = this.pixelY();
-    ellipse(this.px, this.py, 30, 30);
+    let px = this.pixelX();
+    let py = this.pixelY();
+    ellipse(px, py, 30, 30);
+    //Draws the circle for each coordinate
   }
 }
